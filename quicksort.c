b46f234fddf9850580cd572cbae0ee8079bc0144
gfvante@gmail.com <gfvante@gmail.com>
2019-01-06 17:04:30 -0200

    last changes to quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -1,9 +1,10 @@
-/* Sorts a given set of integer values using Quicksort algorithm */
+/* Sort a given set of integer values using Quicksort algorithm */
 
 #include <stdio.h>
 #define N 10
 
 void quicksort(int a[], int low, int high);
+int divide(int a[], int low, int high);
 
 int main(void)
 {
@@ -16,7 +17,7 @@ int main(void)
 	quicksort(a, 0, N - 1);
 
 	printf("In sorted order: ");
-	for (int i = 0; i < N - 1; i++)
+	for (int i = 0; i < N; i++)
 		printf("%d ", a[i]);
 	
 	printf("\n");
@@ -24,7 +25,33 @@ int main(void)
 	return 0;
 }
 
+/* Quicksort - Recursive version */
 void quicksort(int a[], int low, int high)
 {
+	int piv;
 
+	if (low >= high) return;
+	piv = divide(a, low, high);
+	quicksort(a, low, piv - 1);
+	quicksort(a, piv + 1, high);
+}
+
+int divide(int a[], int low, int high)
+{
+	int pElem = a[low];
+
+	for (;;) {
+		while (low < high && pElem <= a[high])
+			high--;
+		if (low >= high) break;
+		a[low++] = a[high];
+
+		while (low < high && a[low] <= pElem)
+			low++;
+		if (low >= high) break;
+		a[high--] = a[low];
+	}
+
+	a[high] = pElem;
+	return high;
 }
