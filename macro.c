35a5ad5a6f246f48b167cf7c89383546b8342848
Gabriel Fioravante <gfvante@gmail.com>
2019-01-20 14:42:11 -0200

    added generic max macro

--- macro.c
+++ macro.c
@@ -2,11 +2,21 @@
 #include <stdlib.h>
 
 #define START int main(int argc, char * argv[])
-#define MAX(x, y) ((x) > (y) ? (x) : (y))
 #define PRINT(str, type) printf(#type "\n", str)
 #define FOR(S) for (int i = 1; i < S; i++)
+
 #define BEGIN {
 #define END }
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+#define GENERIC_MAX(type)		\
+type type##_max(type x, type y)	\
+{								\
+	return x > y ? x : y;		\
+}
+
+GENERIC_MAX(int)
 
 START
 
@@ -15,6 +25,7 @@ BEGIN
 		PRINT(argv[i], %s);
 	
 	PRINT(MAX(20, MAX(10, 12)), %d);
+	PRINT(int_max(20, 10), %d);
 	
 	return EXIT_SUCCESS;
 END
