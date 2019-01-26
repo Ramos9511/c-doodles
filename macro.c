4c783002e4e95b6f9573d94c7602607679a982ba
Gabriel Fioravante <gfvante@gmail.com>
2019-01-25 23:06:59 -0200

    tried some predefined macros

--- macro.c
+++ macro.c
@@ -16,16 +16,30 @@ type type##_max(type x, type y)	\
 	return x > y ? x : y;		\
 }
 
+#define ECHO(s)		\
+	do {			\
+		gets(s);	\
+		puts(s);	\
+	} while (0)
+
 GENERIC_MAX(int)
 
 START
 
 BEGIN
-	FOR(argc)
+/* 	FOR(argc)
 		PRINT(argv[i], %s);
 	
 	PRINT(MAX(20, MAX(10, 12)), %d);
-	PRINT(int_max(20, 10), %d);
+	PRINT(int_max(20, 10), %d); */
+	
+	PRINT("File name: " __FILE__, %s);
+	PRINT("Today is: " __DATE__, %s);
+	PRINT("Compilation time: " __TIME__, %s);
+	PRINT(__STDC__, %d);
+	
+/* 	char s[10];
+	ECHO(s); */
 	
 	return EXIT_SUCCESS;
 END
