d9e233ca11b7e4b72e2d43455234760aa03c5dc2
Gabriel Fioravante <gfvante@gmail.com>
2019-01-26 00:47:40 -0200

    example of macro with variable number of arguments

--- macro.c
+++ macro.c
@@ -22,6 +22,12 @@ type type##_max(type x, type y)	\
 		puts(s);	\
 	} while (0)
 
+#define ADD(x, y) (x + y)
+
+#define TEST(condition, ...) ((condition) ? 	\
+	printf("passed test: %s\n", #condition) : 	\
+	printf(__VA_ARGS__))
+
 GENERIC_MAX(int)
 
 START
@@ -33,13 +39,18 @@ BEGIN
 	PRINT(MAX(20, MAX(10, 12)), %d);
 	PRINT(int_max(20, 10), %d); */
 	
-	PRINT("File name: " __FILE__, %s);
+/* 	PRINT("File name: " __FILE__, %s);
 	PRINT("Today is: " __DATE__, %s);
 	PRINT("Compilation time: " __TIME__, %s);
 	PRINT(__STDC__, %d);
+	PRINT(__STDC_HOSTED__, %d);
+	PRINT(__STDC_VERSION__, %d); */
 	
 /* 	char s[10];
 	ECHO(s); */
+	
+	TEST(3 > 10, "10 is greater than 3 \n");
+	PRINT(__func__, %s);
 	
 	return EXIT_SUCCESS;
 END
