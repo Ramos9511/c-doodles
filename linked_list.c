9226651375b2e169910cb2552524962747db6a89
Gabriel Fioravante <gfvante@gmail.com>
2019-08-04 17:47:40 -0300

    refac and commenting

--- linked_list.c
+++ linked_list.c
@@ -1,113 +1,108 @@
+/***********************************************************************
+*
+* FILENAME:
+*		linked_list.c
+*
+* DESCRIPTION :
+* 		Implementation of Linked list api functions
+*
+* AUTHOR:
+* 		WesternSage - 04 Agus 2019
+*
+***********************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "linked_list.h"
 
-Node *list_new()
-{
-    return NULL;
+Node *list_new() {
+	return NULL;
 }
 
-void list_print(Node *head_ptr)
-{
-    Node *temp = head_ptr;
-    while (temp)
-    {
-        printf("%d ", temp->data);
-        temp = temp->next;
-    }
+void list_print(Node *head_ptr) {
+	Node *temp = head_ptr;
+	while (temp) {
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
 }
 
 // TODO: Implement append func
 
-/* insertion at nth position of list */
 void list_insert(int x, int n, Node **head_ptr)
 {
-    Node *temp = (Node *)malloc(sizeof(Node));
-    temp->data = x;
-    temp->next = NULL;
-    // insertion at first position
-    if (n == 1)
-    {
-        temp->next = *head_ptr;
-        *head_ptr = temp;
-    }
-    // insertion at other positions
-    else
-    {
-        Node *temp2 = (*head_ptr);
-        for (int i = 0; i < n - 2; i++)
-            temp2 = temp2->next;
-        temp->next = temp2->next;
-        temp2->next = temp;
-    }
+	Node *temp = (Node *)malloc(sizeof(Node));
+	temp->data = x;
+	temp->next = NULL;
+	// insertion at first position
+	if (n == 1) {
+		temp->next = *head_ptr;
+		*head_ptr = temp;
+	} else {
+		Node *temp2 = (*head_ptr);
+		for (int i = 0; i < n - 2; i++)
+			temp2 = temp2->next;
+		temp->next = temp2->next;
+		temp2->next = temp;
+	}
 }
 
 void list_remove(int n, Node **head_ptr)
 {
-    Node *temp = *head_ptr;
-    // remove first node
-    if (n == 1)
-    {
-        *head_ptr = temp->next;
-        free(temp);
-    }
-    // nodes in the middle
-    else
-    {
-        for (int i = 0; i < n - 2; i++)
-            temp = temp->next;
-        Node *temp2 = temp->next;
-        temp->next = temp2->next;
-        free(temp2);
-    }
+	Node *temp = *head_ptr;
+	// remove first node
+	if (n == 1) {
+		*head_ptr = temp->next;
+		free(temp);
+	} else {
+		for (int i = 0; i < n - 2; i++)
+			temp = temp->next;
+		Node *temp2 = temp->next;
+		temp->next = temp2->next;
+		free(temp2);
+	}
 }
 
 void list_reverse(Node **head_ptr)
 {
-    Node *current = *head_ptr;
-    Node *prev = NULL;
-    Node *next = NULL;
-    while (current)
-    {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-    }
-    *head_ptr = prev;
+	Node *current = *head_ptr;
+	Node *prev = NULL;
+	Node *next = NULL;
+	while (current) {
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head_ptr = prev;
 }
 
-// recursive reverse print
 void list_rrprint(Node *head_ptr)
 {
-    if (!head_ptr)
-        return;
-    list_rrprint(head_ptr->next);
-    printf("%d ", head_ptr->data);
+	if (!head_ptr)
+		return;
+	list_rrprint(head_ptr->next);
+	printf("%d ", head_ptr->data);
 }
 
-// recursive print
 void list_rprint(Node *head_ptr)
 {
-    if (!head_ptr)
-    {
-        printf("%d ", head_ptr->data);
-        return;
-    }
-    printf("%d ", head_ptr->data);
-    list_rprint(head_ptr->next);
+	if (!head_ptr) {
+		printf("%d ", head_ptr->data);
+		return;
+	}
+	printf("%d ", head_ptr->data);
+	list_rprint(head_ptr->next);
 }
 
-// recursive reverse
 void list_rreverse(Node **head, Node *p)
 {
-    if (p->next == NULL)
-    {
-        (*head) = p;
-        return;
-    }
-    list_rreverse(head, p->next);
-    Node *q = p->next;
-    q->next = p;
-    p->next = NULL;
+	if (p->next == NULL) {
+		(*head) = p;
+		return;
+	}
+	list_rreverse(head, p->next);
+	Node *q = p->next;
+	q->next = p;
+	p->next = NULL;
 }
--- linked_list.h
+++ linked_list.h
@@ -1,19 +1,34 @@
+/***********************************************************************
+*
+* FILENAME:
+*		linked_list.h
+*
+* DESCRIPTION :
+* 		Linked list api functions prototypes
+*		and required data structures (e.g. list Node)
+*
+* AUTHOR:
+* 		WesternSage - 04 Agus 2019
+*
+***********************************************************************/
+
 #ifndef LINKED_LIST
 #define LINKED_LIST
 
-    struct Node
-    {
-        int data;
-        struct Node *next;
-    } typedef Node;
+struct Node {
+	int data;
+	struct Node *next;
+} typedef Node;
 
-    Node *list_new();
-    void list_print(Node *head_ptr);
-    void list_rprint(Node *head_ptr);
-    void list_rrprint(Node *head_ptr);
-    void list_reverse(Node **head_ptr);
-    void list_remove(int n, Node **head_ptr);
-    void list_rreverse(Node **head, Node *p);
-    void list_insert(int x, int n, Node **head_ptr);
+// TODO: Implement append func
+
+Node *list_new();                                   // create empty list
+void list_print(Node *head_ptr);                    // print list
+void list_rprint(Node *head_ptr);                   // recursive print
+void list_rrprint(Node *head_ptr);                  // recursive reverse print
+void list_reverse(Node **head_ptr);                 // revese list
+void list_remove(int n, Node **head_ptr);           // remove item at pos n
+void list_rreverse(Node **head, Node *p);           // recursive reverse
+void list_insert(int x, int n, Node **head_ptr);    // insert x at pos n
 
 #endif
